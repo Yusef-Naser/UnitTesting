8d0e917eb578c1d8bfe6515fa0a72bb5440a1e52
Yusef Naser <yusefnaser@Yusefs-iMac.local>
2021-09-08 16:54:30 +0200

    s

---
+++ UnitTesting/UnitTesting/CoveredClass/CoveredClass.swift
@@ -0,0 +1,20 @@
+//
+//  CoveredClass.swift
+//  UnitTesting
+//
+//  Created by Yusef Naser on 08/09/2021.
+//
+
+import UIKit
+
+class CoveredClass {
+    
+    static func max(_ x: Int, _ y: Int) -> Int {
+        if x < y {
+            return y
+        } else {
+            return x
+        }
+    }
+    
+}
---
+++ UnitTesting/UnitTestingTests/CoveredClassTests/CoveredClassTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoveredClassTests.swift
+//  UnitTestingTests
+//
+//  Created by Yusef Naser on 08/09/2021.
+//
+
+import XCTest
+@testable import UnitTesting
+
+class CoveredClassTests: XCTestCase {
+    
+    func test_zeroTest () {
+        XCTFail("fail covered class")
+    }
+
+}
